94c9a590a43f3be14bc228e65190ead1fda045fc
unknown <emmanuelochigbo60@gmail.com>
2023-10-06 08:41:59 +0100

    testing

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import PinInput from "react-pin-input";
+
+function App() {
+  return (
+    <>
+      hello
+      <PinInput
+        length={4}
+        initialValue=""
+        secret
+        secretDelay={100}
+        onChange={(value, index) => {}}
+        type="numeric"
+        inputMode="number"
+        style={{ padding: "10px" }}
+        inputStyle={{ borderColor: "red" }}
+        inputFocusStyle={{ borderColor: "blue" }}
+        onComplete={(value, index) => {}}
+        autoSelect={true}
+        regexCriteria={/^[ A-Za-z0-9_@./#&+-]*$/}
+      />
+    </>
+  );
+}
+
+export default App;
